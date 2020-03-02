07c15156d5452041864a29fb2521f8b84ad6de1e
Victor Lellis <vmlellis@gmail.com>
2020-03-01 23:52:25 -0300

    Split module in files

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -1,7 +1,14 @@
-
 defmodule Servy.Handler do
-  require Logger
 
+  @moduledoc "Handles HTTP requests."
+
+  @pages_path Path.expand("../../pages", __DIR__)
+
+  import Servy.Plugins, only: [rewrite_path: 1, log: 1, track: 1]
+  import Servy.Parser, only: [parse: 1]
+  import Servy.FileHandler, only: [handle_file: 2]
+
+  @doc "Transforms the request into a response."
   def handle(request) do
     request
     |> parse
@@ -13,76 +20,19 @@ defmodule Servy.Handler do
     |> format_response
   end
 
-  def emojify(%{status: 200, resp_body: resp_body} = conv) do
-    emojis = "😎" |> String.duplicate(5)
-    %{ conv | resp_body: "#{emojis}\n #{resp_body} \n#{emojis}" }
-  end
-
-  def emojify(conv), do: conv
-
-  def track(%{status: 404, path: path} = conv) do
-    Logger.warn "Do we have a problem, Houston?"
-    IO.puts "Warning: #{path} is on the loose!"
-    conv
-  end
-
-  def track(%{status: 403} = conv) do
-    Logger.warn "Do we have a problem, Houston?"
-    conv
-  end
-
-  def track(%{status: 500} = conv) do
-    Logger.error "Danger Will Robinson!"
-    conv
-  end
-
-  def track(%{status: 200} = conv) do
-    Logger.info "It's lunchtime somewhere."
-    conv
-  end
-
-  def track(conv), do: conv
-
-  def rewrite_path(%{method: "GET", path: "/wildlife"} = conv) do
-    %{ conv | path: "/wildthings" }
-  end
-
-  # defp rewrite_path(%{method: "GET", path: "/bears?id=" <> id} = conv) do
-  #   %{ conv | path: "/bears/#{id}" }
-  # end
-
-  def rewrite_path(%{method: "GET", path: path} = conv) do
-    regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
-    captures = Regex.named_captures(regex, path)
-    rewrite_path_captures(conv, captures)
-  end
-
-  def rewrite_path(conv), do: conv
-
-  def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
-    %{ conv | path: "/#{thing}/#{id}" }
-  end
-
-  def rewrite_path_captures(conv, nil), do: conv
-
-  def log(conv), do: IO.inspect conv
-
-  def parse(request) do
-    [method, path, _] =
-      request
-      |> String.split("\n")
-      |> List.first
-      |> String.split(" ")
-
-    %{ method: method, path: path, resp_body: "", status: nil }
-  end
-
   def route(%{method: "GET", path: "/wildthings"} = conv) do
     %{ conv | status: 200, resp_body: "Bears, Lions, Tigers" }
   end
 
   def route(%{method: "GET", path: "/bears"} = conv) do
     %{ conv | status: 200, resp_body: "Teddy, Smokey, and Paddington" }
+  end
+
+  def route(%{method: "GET", path: "/bears/new"} = conv) do
+    @pages_path
+    |> Path.join("form.html")
+    |> File.read
+    |> handle_file(conv)
   end
 
   def route(%{method: "GET", path: "/bears/" <> id} = conv) do
@@ -94,26 +44,21 @@ defmodule Servy.Handler do
   end
 
   def route(%{method: "GET", path: "/about"} = conv) do
-    Path.expand("../../pages", __DIR__)
+    @pages_path
     |> Path.join("about.html")
     |> File.read
     |> handle_file(conv)
   end
 
+  # def route(%{method: "GET", path: "/pages/" <> file} = conv) do
+  #   Path.expand("../../pages", __DIR__)
+  #   |> Path.join(file <> ".html")
+  #   |> File.read
+  #   |> handle_file(conv)
+  # end
+
   def route(%{path: path} = conv) do
     %{ conv | status: 404, resp_body: "No #{path} here!" }
-  end
-
-  def handle_file({:ok, content}, conv) do
-    %{ conv | status: 200, resp_body: content }
-  end
-
-  def handle_file({:error, :enoent}, conv) do
-    %{ conv | status: 404, resp_body: "File not found!" }
-  end
-
-  def handle_file({:error, reason}, conv) do
-    %{ conv | status: 500, resp_body: "File error: #{reason}" }
   end
 
   # def route(%{method: "GET", path: "/about"} = conv) do
@@ -242,6 +187,17 @@ end
 
 request = """
 GET /about HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+response = Servy.Handler.handle(request)
+IO.puts response
+
+request = """
+GET /bears/new HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
---
+++ lib/servy/plugins.ex
@@ -0,0 +1,58 @@
+defmodule Servy.Plugins do
+  require Logger
+
+  def emojify(%{status: 200, resp_body: resp_body} = conv) do
+    emojis = "😎" |> String.duplicate(5)
+    %{ conv | resp_body: "#{emojis}\n #{resp_body} \n#{emojis}" }
+  end
+
+  def emojify(conv), do: conv
+
+  @doc "Logs 404 requests"
+  def track(%{status: 404, path: path} = conv) do
+    Logger.warn "Do we have a problem, Houston?"
+    IO.puts "Warning: #{path} is on the loose!"
+    conv
+  end
+
+  def track(%{status: 403} = conv) do
+    Logger.warn "Do we have a problem, Houston?"
+    conv
+  end
+
+  def track(%{status: 500} = conv) do
+    Logger.error "Danger Will Robinson!"
+    conv
+  end
+
+  def track(%{status: 200} = conv) do
+    Logger.info "It's lunchtime somewhere."
+    conv
+  end
+
+  def track(conv), do: conv
+
+  def rewrite_path(%{method: "GET", path: "/wildlife"} = conv) do
+    %{ conv | path: "/wildthings" }
+  end
+
+  # defp rewrite_path(%{method: "GET", path: "/bears?id=" <> id} = conv) do
+  #   %{ conv | path: "/bears/#{id}" }
+  # end
+
+  def rewrite_path(%{method: "GET", path: path} = conv) do
+    regex = ~r{\/(?<thing>\w+)\?id=(?<id>\d+)}
+    captures = Regex.named_captures(regex, path)
+    rewrite_path_captures(conv, captures)
+  end
+
+  def rewrite_path(conv), do: conv
+
+  def rewrite_path_captures(conv, %{"thing" => thing, "id" => id}) do
+    %{ conv | path: "/#{thing}/#{id}" }
+  end
+
+  def rewrite_path_captures(conv, nil), do: conv
+
+  def log(conv), do: IO.inspect conv
+end
