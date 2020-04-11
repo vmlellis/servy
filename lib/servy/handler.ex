7d286f9e1675feed2d96893fda512232ec9f7fae
Victor Lellis <vmlellis@gmail.com>
2020-04-11 14:25:11 -0300

    Asynchronous Tasks

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -24,21 +24,18 @@ defmodule Servy.Handler do
     |> put_content_length
     |> format_response
   end
-  
-  def route(%Conv{ method: "GET", path: "/snapshots" } = conv) do
-    parent = self() # the requst-handling process
-    
-    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("cam-1")}) end)
-    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("cam-2")}) end)
-    spawn(fn -> send(parent, {:result, VideoCam.get_snapshot("cam-3")}) end)
-    
-    snapshot1 = receive do {:result, filename} -> filename end
-    snapshot2 = receive do {:result, filename} -> filename end
-    snapshot3 = receive do {:result, filename} -> filename end
-  
-    snapshots = [snapshot1, snapshot2, snapshot3]
-  
-    %{ conv | status: 200, resp_body: inspect snapshots}
+
+  def route(%Conv{ method: "GET", path: "/sensors" } = conv) do
+    task = Task.async(fn -> Servy.Tracker.get_location("bigfoot") end)
+
+    snapshots =
+      ["cam-1", "cam-2", "cam-3"]
+      |> Enum.map(&Task.async(fn -> VideoCam.get_snapshot(&1) end))
+      |> Enum.map(&Task.await/1)
+
+    where_is_bigfoot = Task.await(task)
+
+    %{ conv | status: 200, resp_body: inspect {snapshots, where_is_bigfoot} }
   end
 
   def route(%Conv{method: "GET", path: "/kaboom"}) do
