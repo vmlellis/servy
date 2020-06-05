4f04955e5d59a3fd15c65604ed791aca8822b98d
Victor Lellis <vmlellis@gmail.com>
2020-06-05 00:41:06 -0300

    Dynamically change the refresh interval

--- lib/servy/sensor_server.ex
+++ lib/servy/sensor_server.ex
@@ -1,32 +1,49 @@
 defmodule Servy.SensorServer do
 
   @name :sensor_server
-  @refresh_interval :timer.seconds(5) # :time.minutes(60)
+  # @refresh_interval :timer.seconds(5) # :timer.minutes(60)
+
+  defmodule State do
+    defstruct sensor_data: %{},
+              refresh_interval: :timer.minutes(60)
+  end
 
   use GenServer
 
   # Client Interface
 
   def start do
-    GenServer.start(__MODULE__, %{}, name: @name)
+    GenServer.start(__MODULE__, %State{}, name: @name)
   end
 
   def get_sensor_data do
     GenServer.call @name, :get_sensor_data
   end
 
+  def set_refresh_interval(time_in_ms) do
+    GenServer.cast @name, {:set_refresh_interval, time_in_ms}
+  end
+
   # Server Callbacks
 
-  def init(_state) do
-    initial_state = run_tasks_to_get_sensor_data()
-    schedule_refresh()
+  def init(state) do
+    sensor_data = run_tasks_to_get_sensor_data()
+    initial_state = %{state | sensor_data: sensor_data}
+    schedule_refresh(state.refresh_interval)
     {:ok, initial_state}
   end
 
-  def handle_info(:refresh, _state) do
+  def handle_cast({:set_refresh_interval, time_in_ms}, state) do
+    new_state = %{ state | refresh_interval: time_in_ms }
+    schedule_refresh(new_state.refresh_interval)
+    {:noreply, new_state}
+  end
+
+  def handle_info(:refresh, state) do
     IO.puts "Refreshing the cache..."
-    new_state = run_tasks_to_get_sensor_data()
-    schedule_refresh()
+    sensor_data = run_tasks_to_get_sensor_data()
+    new_state = %{state | sensor_data: sensor_data}
+    schedule_refresh(state.refresh_interval)
     {:noreply, new_state}
   end
 
@@ -35,12 +52,12 @@ defmodule Servy.SensorServer do
     {:noreply, state}
   end
 
-  defp schedule_refresh do
-    Process.send_after(self(), :refresh, @refresh_interval)
+  defp schedule_refresh(time_in_ms) do
+    Process.send_after(self(), :refresh, time_in_ms)
   end
 
   def handle_call(:get_sensor_data, _from, state) do
-    {:reply, state, state}
+    {:reply, state.sensor_data, state}
   end
 
   defp run_tasks_to_get_sensor_data do
