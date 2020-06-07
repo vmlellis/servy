7f0fe9f05a5f9176637d0e8e362fb2339234443a
Victor Lellis <vmlellis@gmail.com>
2020-06-07 17:21:54 -0300

    Get the Server Process

--- lib/servy/kick_starter.ex
+++ lib/servy/kick_starter.ex
@@ -1,15 +1,28 @@
 defmodule Servy.KickStarter do
   use GenServer
 
+  # Client Interface
+
   def start do
     IO.puts "Starting the kickstarter..."
     GenServer.start(__MODULE__, :ok, name: __MODULE__)
   end
 
+  def get_server do
+    # Alternative: Process.whereis(:http_server)
+    GenServer.call(__MODULE__, :get_server)
+  end
+
+  # Server Callbacks
+
   def init(:ok) do
     Process.flag(:trap_exit, true)
     server_pid = start_server()
     {:ok, server_pid}
+  end
+
+  def handle_call(:get_server, _from, state) do
+    {:reply, state, state}
   end
 
   def handle_info({:EXIT, _pid, reason}, _state) do
